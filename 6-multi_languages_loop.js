25b3919e9d90e5421715aee7cc9cce514e6edb56
Mary <maryndungu1412@gmail.com>
2025-06-11 19:21:49 +0300

    q6 correction

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,4 +1,4 @@
-let languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i=0; i<languages.length; i++){
     console.log(languages[i]);
 }
